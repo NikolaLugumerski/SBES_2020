b9d9063daa5b9bb7b13414f63a02a8aefd5af434
Bogdan Vilimonovic <bogdan.vilimonovic@gmail.com>
2020-12-19 16:18:41 +0100

    change links algorithm one millimeter almost done

--- PhishingToolkit/PhishingApp/Commands/ChangeLinksCommand.cs
+++ PhishingToolkit/PhishingApp/Commands/ChangeLinksCommand.cs
@@ -9,6 +9,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace PhishingApp.Commands
 {
@@ -38,16 +39,28 @@ namespace PhishingApp.Commands
 		public void Execute(object parameter)
 		{
 			string html = EmailModel.Body;
+			/*
 			StringBuilder newHtml = new StringBuilder(html);
-			Regex r = new Regex(@"\<a href=\""([^\""]+)\"">([^<]+)"); // 1st capture for the replacement and 2nd for the find
+			Regex r = new Regex(@"\<a href=\""([^\""]+)\"">"); // 1st capture for the replacement and 2nd for the find
 			foreach (var match in r.Matches(html).Cast<Match>().OrderByDescending(m => m.Index))
 			{
 				string text = EmailModel.MaliciousLink;
 				string newHref = DBTranslate(text);
-				newHtml.Remove(match.Groups[1].Index, match.Groups[1].Length);
-				newHtml.Insert(match.Groups[1].Index, newHref);
+				newHtml.Remove(match.Groups[0].Index, match.Groups[0].Length);
+				newHtml.Insert(match.Groups[0].Index, newHref);
 			}
-
+			EmailModel.Body = newHtml.ToString();
+			*/
+			HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+			htmlDoc.LoadHtml(EmailModel.Body);
+			var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//a");
+			for(int i=0; i<htmlNodes.Count; i++)
+            {
+				htmlNodes[i].Attributes["href"].Value = EmailModel.MaliciousLink;
+				
+            }
+			//treba vratiti ove htmlNodes u htmlDoc
+			EmailModel.Body = htmlDoc.Text;
 		}
 
 		static string DBTranslate(string s)
